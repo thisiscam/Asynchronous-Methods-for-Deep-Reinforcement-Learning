78f43455ce398a756c7dbbc1c0555f0bd984e2ff
Jianqiao Yang <jq.yang@berkeley.edu>
2016-04-23 23:14:44 -0700

    save

--- asynchronous_one_step_Q_learning.py
+++ asynchronous_one_step_Q_learning.py
@@ -73,7 +73,7 @@ def bias_variable(shape):
     return tf.Variable(initial)
 
 def conv2d(x, W, stride):
-    return tf.nn.conv2d(x, W, strides = [1, stride, stride, 1], padding = "SAME")
+    return tf.nn.conv2d(x, W, strides = [1, stride, stride, 1], padding = "VALID")
 
 def max_pool_2x2(x):
     return tf.nn.max_pool(x, ksize = [1, 2, 2, 1], strides = [1, 2, 2, 1], padding = "SAME")
@@ -89,10 +89,10 @@ def createNetwork():
     W_conv3 = weight_variable([3, 3, 64, 64])
     b_conv3 = bias_variable([64])
 
-    W_fc1 = weight_variable([256, 256])
-    b_fc1 = bias_variable([256])
+    W_fc1 = weight_variable([3136, 512])
+    b_fc1 = bias_variable([512])
 
-    W_fc2 = weight_variable([256, ACTIONS])
+    W_fc2 = weight_variable([512, ACTIONS])
     b_fc2 = bias_variable([ACTIONS])
 
     # input layer
@@ -100,15 +100,15 @@ def createNetwork():
 
     # hidden layers
     h_conv1 = tf.nn.relu(conv2d(s, W_conv1, 4) + b_conv1)
-    h_pool1 = max_pool_2x2(h_conv1)
+    # h_pool1 = max_pool_2x2(h_conv1)
 
-    h_conv2 = tf.nn.relu(conv2d(h_pool1, W_conv2, 2) + b_conv2)
-    h_pool2 = max_pool_2x2(h_conv2)
+    h_conv2 = tf.nn.relu(conv2d(h_conv1, W_conv2, 2) + b_conv2)
+    # h_pool2 = max_pool_2x2(h_conv2)
 
-    h_conv3 = tf.nn.relu(conv2d(h_pool2, W_conv3, 1) + b_conv3)
-    h_pool3 = max_pool_2x2(h_conv3)
+    h_conv3 = tf.nn.relu(conv2d(h_conv2, W_conv3, 1) + b_conv3)
+    # h_pool3 = max_pool_2x2(h_conv3)
 
-    h_pool3_flat = tf.reshape(h_pool3, [-1, 256])
+    h_pool3_flat = tf.reshape(h_conv3, [-1, 3136])
 
     h_fc1 = tf.nn.relu(tf.matmul(h_pool3_flat, W_fc1) + b_fc1)
 
